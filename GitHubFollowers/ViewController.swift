54d7fd2ea8a4f111e6f9b9add375e831fe20e49a
fredlc <fredlefevre@outlook.com>
2020-02-09 14:26:35 -0500

    Initial commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Fred Lefevre on 2020-02-09.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
