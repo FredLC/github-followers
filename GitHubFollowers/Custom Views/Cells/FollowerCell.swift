793f990ebbbe4af9857b2fdd3a897a875dffb87f
fredlc <fredlefevre@outlook.com>
2020-02-11 11:02:51 -0500

    Created follower cell and custom avatar image view

---
+++ GitHubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,45 @@
+//
+//  FollowerCell.swift
+//  GitHubFollowers
+//
+//  Created by Fred Lefevre on 2020-02-11.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    static let reuseId = "FollowerCell"
+    let avatarImage = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(avatarImage)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImage.heightAnchor.constraint(equalTo: avatarImage.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+}
