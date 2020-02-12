e9c6cef4db91200fe4a38776734e6302a6177f47
fredlc <fredlefevre@outlook.com>
2020-02-12 15:01:11 -0500

    Built and integrated UserInfoHeaderVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Fred Lefevre on 2020-02-12.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
