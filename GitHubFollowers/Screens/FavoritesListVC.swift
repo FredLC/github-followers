bc4d620b32cb5597da8f000cb87cd3a6e55325f0
fredlc <fredlefevre@outlook.com>
2020-02-13 16:31:53 -0500

    Configured add to favorites button

--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -12,7 +12,16 @@ class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        
+        PersistenceManager.retrieveFavorites { (result) in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 
 }
