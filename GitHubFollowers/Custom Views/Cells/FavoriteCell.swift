76843fd0fb8488495b478e3ab174d4e3546518d1
fredlc <fredlefevre@outlook.com>
2020-02-15 11:47:27 -0500

    Configured favorite cell

---
+++ GitHubFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,51 @@
+//
+//  FavoriteCell.swift
+//  GitHubFollowers
+//
+//  Created by Fred Lefevre on 2020-02-15.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    
+    static let reuseId = "FavoriteCell"
+    let avatarImage = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 26)
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favorite: Follower) {
+        usernameLabel.text = favorite.login
+        avatarImage.downloadImage(fromUrl: favorite.avatarUrl)
+    }
+    
+    private func configure() {
+        addSubview(avatarImage)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImage.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            avatarImage.widthAnchor.constraint(equalToConstant: 60),
+            avatarImage.heightAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+}
